9a800e17ee11f487b413eb9adea45c6f335338e4
andr777 <andr.root@gmail.com>
2022-09-27 19:26:15 +0300

    0

---
+++ src/main/java/pro/sky/HomeWork18StreamAPI/HomeWork18StreamApiApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeWork18StreamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork18StreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork18StreamApiApplication.class, args);
+	}
+
+}
